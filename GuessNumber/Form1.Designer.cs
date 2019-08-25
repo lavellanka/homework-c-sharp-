d14ced3fd60a202a9a679e0a486d8acde6d1fbc0
lavellanka <mrs.svirgstina@gmail.com>
2019-08-26 00:39:36 +0300

    Добавьте файлы проекта.

---
+++ GuessNumber/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace УгадайЧисло
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.butExit = new System.Windows.Forms.Button();
+            this.butPlayRestart = new System.Windows.Forms.Button();
+            this.tBoxMyNumber = new System.Windows.Forms.TextBox();
+            this.butTry = new System.Windows.Forms.Button();
+            this.labelCountTry = new System.Windows.Forms.Label();
+            this.labelMoreLess = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(294, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Я загадал число от 0 до 100. Угадай!";
+            // 
+            // butExit
+            // 
+            this.butExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.butExit.Location = new System.Drawing.Point(12, 130);
+            this.butExit.Name = "butExit";
+            this.butExit.Size = new System.Drawing.Size(131, 28);
+            this.butExit.TabIndex = 2;
+            this.butExit.Text = "Выход";
+            this.butExit.UseVisualStyleBackColor = true;
+            this.butExit.Click += new System.EventHandler(this.butExit_Click);
+            // 
+            // butPlayRestart
+            // 
+            this.butPlayRestart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.butPlayRestart.Location = new System.Drawing.Point(196, 130);
+            this.butPlayRestart.Name = "butPlayRestart";
+            this.butPlayRestart.Size = new System.Drawing.Size(131, 28);
+            this.butPlayRestart.TabIndex = 3;
+            this.butPlayRestart.Text = "Играть";
+            this.butPlayRestart.UseVisualStyleBackColor = true;
+            this.butPlayRestart.Click += new System.EventHandler(this.butPlayRestart_Click);
+            // 
+            // tBoxMyNumber
+            // 
+            this.tBoxMyNumber.Enabled = false;
+            this.tBoxMyNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tBoxMyNumber.Location = new System.Drawing.Point(12, 56);
+            this.tBoxMyNumber.MaxLength = 10;
+            this.tBoxMyNumber.Name = "tBoxMyNumber";
+            this.tBoxMyNumber.Size = new System.Drawing.Size(92, 26);
+            this.tBoxMyNumber.TabIndex = 4;
+            this.tBoxMyNumber.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tBoxMyNumber_KeyPress);
+            // 
+            // butTry
+            // 
+            this.butTry.Enabled = false;
+            this.butTry.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.butTry.Location = new System.Drawing.Point(110, 56);
+            this.butTry.Name = "butTry";
+            this.butTry.Size = new System.Drawing.Size(89, 26);
+            this.butTry.TabIndex = 5;
+            this.butTry.Text = "Проверить";
+            this.butTry.UseVisualStyleBackColor = true;
+            this.butTry.Click += new System.EventHandler(this.butTry_Click);
+            // 
+            // labelCountTry
+            // 
+            this.labelCountTry.AutoSize = true;
+            this.labelCountTry.Enabled = false;
+            this.labelCountTry.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelCountTry.Location = new System.Drawing.Point(13, 97);
+            this.labelCountTry.Name = "labelCountTry";
+            this.labelCountTry.Size = new System.Drawing.Size(150, 18);
+            this.labelCountTry.TabIndex = 6;
+            this.labelCountTry.Text = "Сделано попыток: 0";
+            // 
+            // labelMoreLess
+            // 
+            this.labelMoreLess.AutoSize = true;
+            this.labelMoreLess.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelMoreLess.Location = new System.Drawing.Point(205, 62);
+            this.labelMoreLess.Name = "labelMoreLess";
+            this.labelMoreLess.Size = new System.Drawing.Size(0, 16);
+            this.labelMoreLess.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 170);
+            this.Controls.Add(this.labelMoreLess);
+            this.Controls.Add(this.labelCountTry);
+            this.Controls.Add(this.butTry);
+            this.Controls.Add(this.tBoxMyNumber);
+            this.Controls.Add(this.butPlayRestart);
+            this.Controls.Add(this.butExit);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Угадай число";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button butExit;
+        private System.Windows.Forms.Button butPlayRestart;
+        private System.Windows.Forms.TextBox tBoxMyNumber;
+        private System.Windows.Forms.Button butTry;
+        private System.Windows.Forms.Label labelCountTry;
+        private System.Windows.Forms.Label labelMoreLess;
+    }
+}
+
---
+++ GuessNumber/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace УгадайЧисло
+{
+    public partial class Form1 : Form
+    {
+        private Random rand = new Random(); // переменная для получения случайного числа
+        private int number; // число которое нужно угадать (случайное)
+        private int countTry = 0; // счётчик попыток пользователя угадать
+
+        public Form1() // конструктор формы
+        {
+            InitializeComponent(); // вызываем функцию инициализации компонентов окна
+        }
+
+        private void butExit_Click(object sender, EventArgs e)
+        {
+            this.Close(); // закрываем игру
+        }
+
+        private void butPlayRestart_Click(object sender, EventArgs e)
+        {
+            // если надпись на кнопке "Игры или Рестарта" написано "Играть"
+            // то есть пользователь только включил игру и нажал кнопку Играть
+            if (butPlayRestart.Text == "Играть") 
+            {
+                number = rand.Next(100); // снова загадываем случайное число от 0 до 100
+                // переписываем надпись на кнопке, что бы можно было
+                butPlayRestart.Text = "Переиграть"; // занаво начать игру той же кнопкой
+                // включаем для использования 
+                labelCountTry.Enabled = true; // лейбочку подсчёта попыток
+                tBoxMyNumber.Enabled = true; // и поле ввода числа
+                labelMoreLess.Text = ""; // чистим лейбочку индикации больше число или меньше
+            }
+            else // сюда мы попадаем когда хотим переиграть и нажимаем на кнопку "Переиграть"
+            {
+                number = rand.Next(100); // снова загадываем случайное число от 0 до 100
+                countTry = 0; // обнуляем счётчик подсчёта попыток игрока угадать
+                labelCountTry.Text = "Сделано попыток: 0"; // обнуляем вывод попыток
+                tBoxMyNumber.Text = ""; // чистим поле ввода числа
+                butTry.Enabled = false; // выключаем кнопку "Проверить"
+                labelMoreLess.Text = ""; // чистим лейбачку индикации больше чило или меньше
+            }
+        }
+
+        private void tBoxMyNumber_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // проверяем нажатую клавишу
+            // если она не есть число или управляющая тогда указываем что 
+            // это событие обработано и нажатый символ не выводиться
+            // (по опыту управляющий символ - Backspace, другие управляющие клавиши я не нашёл)
+            if (!Char.IsDigit(e.KeyChar) && (!Char.IsControl(e.KeyChar)))
+            {
+                e.Handled = true;
+            }
+            else
+            {
+                // включаем кнопку "Проверить"
+                butTry.Enabled = true;
+            }
+        }
+
+        private void butTry_Click(object sender, EventArgs e)
+        {
+            butTry.Enabled = false; // выключаем кнопку "Проверить"
+            // переводим введенную строку в число
+            int inputNumber = Convert.ToInt32(tBoxMyNumber.Text); 
+            // проверяем введённое значение в textBox что бы оно не превышало границы (100)
+            if (inputNumber > 100)
+            {
+                MessageBox.Show("Вы ввели больше значение чем можно. Введите другое",
+                    "Угадай число", MessageBoxButtons.OK);
+                tBoxMyNumber.Text = ""; // чистим textBox
+            }
+            else
+            {
+                labelCountTry.Text = "Сделано попыток: " + (++countTry).ToString(); // считаем попытки
+                if (inputNumber > number)  // проверяем больше ли введёное чило от загаданого
+                    labelMoreLess.Text = "Ваше число больше"; // и выводим результат сравнивания
+                else if (inputNumber < number) // проверяем меньше ли введёное число
+                    labelMoreLess.Text = "Ваше число меньше"; // и выодим результат
+                else // сюда попадаем когда мы угадали число
+                {
+                    MessageBox.Show("          Поздравляем!   \n" +
+                                                 "Вы угадали число за " + countTry.ToString() + " попыток"
+                                                 , "Угадай число",
+                                                 MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    tBoxMyNumber.Text = ""; // чистим поле ввода чисел
+                    // чистим лейбл индикации больше введённое число или меньше
+                    labelMoreLess.Text = ""; 
+                    countTry = 0; // обнуляем счётчик попыток игрока
+                    labelCountTry.Text = "Сделано попыток: 0"; // обнуляем вывод попыток
+                    labelCountTry.Enabled = false; // делаем не доступным лейбл попыток
+                    tBoxMyNumber.Enabled = false; // делаем не доступным поле ввода числа
+                    butPlayRestart.Text = "Играть"; // меняем натпись на кнопке Играть
+                    
+                }
+            }
+        }
+    }
+}
